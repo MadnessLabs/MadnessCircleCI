7fbb8c5b9de936799355310642f37543970a3efd
Popcorn245 <Bobby@madnesslabs.net>
2019-01-25 16:43:35 -0600

    Change text on home page

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -16,10 +16,7 @@ export class AppHome {
 
       <ion-content padding>
         <p>
-          Welcome to the PWA Toolkit. You can use this starter to build entire
-          apps with web components using Stencil and ionic/core! Check out the
-          README for everything that comes in this starter out of the box and
-          check out our docs on <a href="https://stenciljs.com">stenciljs.com</a> to get started.
+          We freakin did set Continuous Integration. 
         </p>
 
         <ion-button href="/profile/ionic" expand="block">Profile page</ion-button>
